ea1c1fc4c966e548b48a00e5e1465e065d1a5552
Jepsens1 <jepsens123@outlook.dk>
2022-11-09 12:43:32 +0100

    Add project files.

---
+++ H4SymmetriskKryptering/CryptoService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H4SymmetriskKryptering
+{
+    internal class CryptoService
+    {
+        private SymmetricAlgorithm _symmetric;
+
+        public CryptoService(string cipher)
+        {
+            Generate(cipher);
+        }
+        private void Generate(string cipher)
+        {
+            switch (cipher.ToLower())
+            {
+                case "des":
+                    _symmetric = DES.Create();
+                    _symmetric.Key = GenerateRandomByteArray(8);
+                    _symmetric.IV = GenerateRandomByteArray(8);
+                    break;
+                case "3des":
+                    _symmetric = TripleDES.Create();
+                    _symmetric.Key = GenerateRandomByteArray(24);
+                    _symmetric.IV = GenerateRandomByteArray(8);
+                    break;
+                case "aes":
+                    _symmetric = Aes.Create();
+                    _symmetric.Key = GenerateRandomByteArray(32);
+                    _symmetric.IV = GenerateRandomByteArray(16);
+                    break;
+            }
+
+        }
+        public byte[] GetKey()
+        {
+            return _symmetric.Key;
+        }
+        public byte[] GetIV()
+        {
+            return _symmetric.IV;
+        }
+        private byte[] GenerateRandomByteArray(int size)
+        {
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                byte[] buffer = new byte[size];
+                rng.GetBytes(buffer);
+                return buffer;
+            }
+        }
+        public byte[] Encrypt(byte[] mess)
+        {
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, _symmetric.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(mess, 0, mess.Length);
+            cs.Close();
+            return ms.ToArray();
+        }
+        public byte[] Decrypt(byte[] mess)
+        {
+            byte[] plaintext = new byte[mess.Length];
+            MemoryStream ms = new MemoryStream(mess);
+            CryptoStream cs = new CryptoStream(ms, _symmetric.CreateDecryptor(), CryptoStreamMode.Read);
+            cs.Read(plaintext, 0, mess.Length);
+            cs.Close();
+            return plaintext;
+
+        }
+    }
+}
---
+++ H4SymmetriskKryptering/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace H4SymmetriskKryptering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Display("aes");
+            Display("des");
+            Display("3des");
+            Console.ReadKey();
+        }
+        static void Display(string method)
+        {
+            Stopwatch watch = Stopwatch.StartNew();
+            CryptoService crypto = new CryptoService(method);
+            byte[] encrypted = crypto.Encrypt(Encoding.UTF8.GetBytes("Hello World"));
+            Console.WriteLine($"Encrypted message: {Encoding.UTF8.GetString(encrypted)}");
+            byte[] decrypted = crypto.Decrypt(encrypted);
+            watch.Stop();
+            byte[] key = crypto.GetKey();
+            byte[] iv = crypto.GetIV();
+            Console.WriteLine($"Decrypted message: {Encoding.UTF8.GetString(decrypted)}\nKey: {Encoding.UTF8.GetString(key)}" +
+                $"\nIV:  {Encoding.UTF8.GetString(iv)}\nTime it Took: {watch.Elapsed.Milliseconds}ms\n");
+        }
+    }
+}
